43d4b0544944ece9ff7da6b16a7afc8388dec4d7
kraus <olikraus@gmail.com>
2022-05-14 10:07:52 +0200

    update

---
+++ spi_ws2812b/spi_ws2812b.ino
@@ -0,0 +1,175 @@
+/*
+  spi_ws2812b.ino
+
+  SystemCoreClock: 168 MHz
+  APB2: 84Mhz 
+
+  SPI Clock BR = 100 --> Clock Divide by 32 --> 84 / 32 = 2.625 MHz --> 380ns
+
+  per bit:
+    0: 1000 --> 380ns pulse
+    1: 1100 --> 760ns pulse
+  
+*/
+
+struct _ws2812_spi
+{
+  volatile uint8_t *spi_data;
+  volatile uint32_t byte_cnt;    // remaining bytes
+  volatile uint32_t b;    // the current byte
+  volatile uint32_t bit_cnt;
+  volatile uint32_t post_data_wait;
+  volatile uint32_t isr_ticks;
+  volatile uint32_t max_isr_ticks;
+  volatile int in_progress;
+};
+typedef struct _ws2812_spi ws2812_spi_t;
+
+ws2812_spi_t ws2812_spi;
+
+
+void ws2812_spi_init(void)
+{
+  RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+  delay(1);
+  SPI1->CR1 = 0;  /* disable SPI */
+  SPI1->I2SCFGR = 0;  /* no I2S */
+  SPI1->CR2 = 0; /* disable interrupts */
+
+  /* PA7, AF5 --> SPI1 MOSI */
+
+  GPIOA->MODER &= ~GPIO_MODER_MODER7;
+  GPIOA->MODER |= GPIO_MODER_MODER7_1; /* alternate function mode */
+
+  GPIOA->OTYPER &= ~GPIO_OTYPER_OT7;  /* push pull */
+  GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD7;  /* no pullup/pulldown */
+  
+  GPIOA->AFR[0] &= ~GPIO_AFRL_AFSEL7;
+  GPIOA->AFR[0] |= GPIO_AFRL_AFSEL7_2 | GPIO_AFRL_AFSEL7_0; /* configure AF5, bitmask 0101 */
+
+  NVIC_SetPriority(SPI1_IRQn, 2);  /* 0: highes priority, 3: lowest priority */
+  NVIC_EnableIRQ(SPI1_IRQn);    
+}
+
+
+/*
+  This SPI handler will do an online conversion of the bit values to the WS2812B format 
+  4 bits of data are converted into two bytes (16 bit) of data
+  This procedure is time critical: 64 Sys-Clock-Cycle per SPI bit 64*16 = 1024 Clock Cycles
+  ==> Upper Limit for this procedure are 1024 clock cycles
+*/
+void __attribute__ ((interrupt)) SPI1_IRQHandler(void)
+{
+  //uint32_t start = SysTick->VAL;
+  //uint32_t end;
+  if ( ws2812_spi.byte_cnt > 0 || ws2812_spi.bit_cnt > 0 )
+  {
+    uint32_t d = 0x4444;
+    register uint32_t b;
+    
+    if ( ws2812_spi.bit_cnt == 0 )
+    {
+      ws2812_spi.b = *ws2812_spi.spi_data++;
+      ws2812_spi.byte_cnt--;
+      ws2812_spi.bit_cnt = 2;
+    }
+    
+    b = ws2812_spi.b;
+    if ( b & 128 )
+      d |= 0x2000;
+    if ( b & 64 )
+      d |= 0x0200;
+    if ( b & 32 )
+      d |= 0x0020;
+    if ( b & 16 )
+      d |= 0x0002;
+    b <<= 4;
+    ws2812_spi.b = b;
+    
+    SPI1->DR = d;
+    ws2812_spi.bit_cnt--;
+  }
+  else
+  {
+    if ( ws2812_spi.post_data_wait > 0 )
+    {
+      /* wait for 50us, this are 125 SPI clocks (each is 0.4us) --> send 128 bits, 8x 16 Bit words */
+      SPI1->DR = 0;
+      ws2812_spi.post_data_wait--;
+    }
+    else
+    {      
+      /* ensure, that the SCK goes to low after the byte transfer: */
+      /* Set the EOT flag at the end of the transfer */
+      
+      /* not used */
+      
+      /* disable interrupt, needs to be re-enabled whenever new data should be transmitted */
+      SPI1->CR2 = 0;
+      ws2812_spi.in_progress = 0;
+    }
+  } 
+  /*
+  end = SysTick->VAL;
+  if ( start < end )
+    start += SysTick->LOAD;
+  start -= end;      // calculate the duration in ticks
+  if ( ws2812_spi.max_isr_ticks < start )
+    ws2812_spi.max_isr_ticks = start; // calculate maximum
+  ws2812_spi.isr_ticks = start;
+  */
+}
+
+void ws2812_spi_out(uint8_t *data, int cnt)
+{
+  
+  /* wait until data is transmitted */
+  while( ws2812_spi.in_progress != 0 )
+    ;
+
+
+  SPI1->CR1 = 0;    /* disable SPI1 */
+  
+  SPI1->CR1 = 0
+    | SPI_CR1_DFF   /* select 16 bit data format */
+    | SPI_CR1_BR_2  /* 100: divide by 32 */
+    | SPI_CR1_MSTR  /* master transmit */
+    ;
+  
+  SPI1->CR2 = 0
+    | SPI_CR2_TXEIE /* buffer empty interrupt */
+    ;
+  
+  ws2812_spi.spi_data = data;
+  ws2812_spi.byte_cnt = cnt;
+  ws2812_spi.bit_cnt = 0;
+  ws2812_spi.post_data_wait = 9;    /* 8 would be sufficient, use 9 to be on the safe side */
+  ws2812_spi.in_progress  = 1;
+
+  SPI1->CR1 |= SPI_CR1_SPE;   /* enable SPI */
+
+  /* load first byte, so that the TXRDY interrupt will be generated */
+  /* this is just a zero byte and is ignored by the WS2812B */
+  SPI1->DR = 0;  
+
+}
+
+/*=======================================================================*/
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(PA1, OUTPUT);
+  ws2812_spi_init();
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  uint8_t a[3] = { 0xff, 0x00, 0x00 };
+  digitalWrite(PA1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(PA1, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);                       // wait for a second
+  ws2812_spi_out(a, 3);
+}
---
+++ sysinfo/sysinfo.ino
@@ -0,0 +1,128 @@
+/*
+  sysinfo
+
+  /.arduino15/packages/STMicroelectronics/hardware/stm32/2.2.0/system/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h
+
+  SystemCoreClockUpdate()
+  
+*/
+
+#include "stm32f4xx_hal.h"
+
+uint32_t prex[8] = { 0, 0, 0, 0, 2, 4, 8, 16 };
+
+void configure160(void)
+{
+  RCC_OscInitTypeDef  RCC_OscInitStruct;
+  
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_OFF;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 160;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+  }
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 160;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+  }
+  SystemCoreClockUpdate();
+
+}
+
+void sysinfo(void)
+{
+  Serial.print("SystemCoreClock = ");
+  Serial.print(SystemCoreClock/1000000UL, DEC);
+  Serial.println(" MHz");
+
+  Serial.print("HPRE (AHB Prescaler Bits) = ");
+  Serial.print((RCC->CFGR & RCC_CFGR_HPRE) >> RCC_CFGR_HPRE_Pos, BIN);
+  Serial.println("");
+
+  Serial.print("PRE1 (APB1 Prescaler Bits) = ");
+  Serial.print((RCC->CFGR & RCC_CFGR_PPRE1) >> RCC_CFGR_PPRE1_Pos, BIN);
+  Serial.print(" / ");
+  Serial.print(SystemCoreClock/1000000UL / prex[(RCC->CFGR & RCC_CFGR_PPRE1) >> RCC_CFGR_PPRE1_Pos], DEC);
+  Serial.println(" MHz");
+
+  Serial.print("PRE2 (APB2 Prescaler Bits) = ");
+  Serial.print((RCC->CFGR & RCC_CFGR_PPRE2) >> RCC_CFGR_PPRE2_Pos, BIN);
+  Serial.print(" / ");
+  Serial.print(SystemCoreClock/1000000UL / prex[(RCC->CFGR & RCC_CFGR_PPRE2) >> RCC_CFGR_PPRE2_Pos], DEC);
+  Serial.println(" MHz");
+
+
+  if ( RCC->APB2ENR & RCC_APB2ENR_SPI1EN )
+    Serial.println("SPI1 @APB2 clock enabled");
+
+  if ( RCC->APB2ENR & RCC_APB2ENR_TIM1EN )
+    Serial.println("TIM1 @APB2 clock enabled");
+
+  if ( RCC->APB1ENR & RCC_APB1ENR_TIM2EN )
+    Serial.println("TIM2 @APB1 clock enabled");
+
+  if ( RCC->APB1ENR & RCC_APB1ENR_TIM3EN )
+    Serial.println("TIM3 @APB1 clock enabled");
+
+  if ( RCC->APB1ENR & RCC_APB1ENR_TIM4EN )
+    Serial.println("TIM4 @APB1 clock enabled");
+
+  if ( RCC->APB1ENR & RCC_APB1ENR_TIM5EN )
+    Serial.println("TIM5 @APB1 clock enabled");
+
+  if ( RCC->APB1ENR & RCC_APB1ENR_TIM6EN )
+    Serial.println("TIM6 @APB1 clock enabled");
+
+  if ( RCC->APB1ENR & RCC_APB1ENR_TIM7EN )
+    Serial.println("TIM7 @APB1 clock enabled");
+
+  if ( RCC->APB2ENR & RCC_APB2ENR_TIM8EN )
+    Serial.println("TIM8 @APB2 clock enabled");
+    
+  if ( RCC->APB2ENR & RCC_APB2ENR_TIM9EN )
+    Serial.println("TIM9 @APB2 clock enabled");
+
+  if ( RCC->APB2ENR & RCC_APB2ENR_TIM10EN )
+    Serial.println("TIM10 @APB2 clock enabled");
+
+  if ( RCC->APB2ENR & RCC_APB2ENR_TIM11EN )
+    Serial.println("TIM11 @APB2 clock enabled");
+
+}
+
+// the setup function runs once when you press reset or power the board
+void setup(void) {
+
+  //configure160();
+  
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(PA1, OUTPUT);
+
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(PA1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(PA1, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+  sysinfo();
+}
