a4e76dc1d0b941e1b733e7333e5f019330bc6751
kraus <olikraus@gmail.com>
2022-06-12 22:57:13 +0200

    update

--- touch_keys/touch_keys.ino
+++ touch_keys/touch_keys.ino
@@ -528,9 +528,11 @@ void setup(void) {
 
   
   Serial.begin(9600);
+  /*
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+  */
 
   buildTouchMeasureList();
   initLEDMatrix();
